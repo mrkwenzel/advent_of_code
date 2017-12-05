9769d4b8a191b170aedaefa9dcd20a13f1675c0c
Markus Wenzel <mrkwenzel@gmail.com>
2017-12-05 08:24:57 +0100

    added day 5

---
+++ 5/instruction_jumper_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+file = open('puzzle_input', 'r')
+puzzle_input = file.read()
+file.close()
+puzzle_input_lines = puzzle_input.split('\n')
+
+steps = 0
+current_step = 0
+
+while True:
+    try:
+        next_step = current_step + int(puzzle_input_lines[current_step])
+        if int(puzzle_input_lines[current_step]) >= 3:
+            puzzle_input_lines[current_step] = int(puzzle_input_lines[current_step]) - 1
+        else:
+            puzzle_input_lines[current_step] = int(puzzle_input_lines[current_step]) + 1
+        current_step = next_step
+        steps += 1
+    except IndexError:
+        break
+
+print(steps)
